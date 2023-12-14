d8c2efdcb6013f9f6a81b8c2a0d5fe6b31e58ce4
Sanli José Bayer <sanli.bayer@alunos.sc.senac.br>
2023-12-14 20:04:05 -0300

    comitando

---
+++ AjudaAiAPI/Entity/CityEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace AjudaAiAPI.Entity
+{
+    public class CityEntity
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public int StateId { get; set; }
+    }
+}
